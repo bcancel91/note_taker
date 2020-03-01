5ece32f6b67ed980e22f5663a935952396698b92
Brian Cancel <brian.e.cancel@gmail.com>
2020-02-29 19:20:19 -0500

    made get and post routes

--- index1.js
+++ index1.js
@@ -10,6 +10,10 @@ port = 3000;
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json())
+
 
 app.get('/notes', function(req, res) {
     
@@ -19,35 +23,49 @@ app.get('/notes', function(req, res) {
 app.get('/api/notes', function(req, res) {
     fs.readFile(`${__dirname}/db/db.json`, 'utf8', (err, data) => {
         if (err) {
-          console.error(err)
+        //  console.error(err)
           return
         }
-        console.log(data)
+       // console.log(data)
+       
+        return res.json(JSON.parse(data))
+
       })
 
 }) 
+app.post('/api/notes', function(req, res){
+ // console.log(req)
+const requestData = req.body;
+
+console.log(requestData);
+
+fs.readFile(`${__dirname}/db/db.json`, 'utf8', (err, data) => {
+    if (err) {
+     // console.error(err);
+    }
+   let dataObject = JSON.parse(data);
+  requestData.id = dataObject.length + "a";
+   dataObject.push(requestData);
+   let dataString = JSON.stringify(dataObject);
+   fs.writeFile(`${__dirname}/db/db.json`, dataString, (err) => 
+   {
+   if(err){
+   console.log(err);
+   }
+   return res.json(dataObject);
+    } )
+  })
+})
+
+
+
 app.get('/*', function(req, res) {
-    res.sendFile(`${__dirname}/public/index.html`)
+    res.sendFile(`${__dirname}/public/index.html`);
+
 }) 
-
-
-
-
 
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-
-
-
-
-
-
-
-
-
-
 
 
